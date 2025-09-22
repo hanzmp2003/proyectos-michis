3fe8b8fdeb4155dd67f6de1f2a9d46e07c40c518
Hanz Madrigal <hanzmadrigalporras@gmail.com>
2025-09-21 18:32:54 -0600

    Creación de las clases básicas

---
+++ Pokemon.java/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Pokemon pokemon = new Pokemon();
+        Jugador jugador = new Jugador();
+        Gimasio gimasio = new Gimasio();
+        Ataque ataque = new Ataque();
+        Elemento elemento = new Elemento();
+    }
+}
--- Pokemon.java/pokemon.java
+++ Pokemon.java/pokemon.java
@@ -1,3 +1,3 @@
-public class pokemon {
+public class Pokemon {
     
 }
