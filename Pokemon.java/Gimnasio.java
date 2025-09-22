c602ca53bc43d696f5dd9e27b0e25a6d638413c3
Emanuel287 <emanuel.sancho@ucr.ac.cr>
2025-09-21 19:44:06 -0600

    le agregué lo que faltaba a la clase Gimnasio

--- Pokemon.java/Gimnasio.java
+++ Pokemon.java/Gimnasio.java
@@ -1,6 +1,14 @@
 
-public class Gimnasion{
-    
+public class Gimnasio{
+ private String nombre;
+ private String tipo; 
+ private int nivel;
+
+public Gimnasio(String nombre, String tipo, int nivel){
+    this.nombre=nombre;
+    this.tipo=tipo;
+    this.nivel=nivel;
 
 
+}
 }
