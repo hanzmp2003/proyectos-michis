cf14d1c8d72637d20f8fe26bb9dbd6588d44d51b
Emanuel287 <emanuel.sancho@ucr.ac.cr>
2025-09-28 09:41:32 -0600

    Agregué setters y getters

--- Pokemon.java/Lideres.java
+++ Pokemon.java/Lideres.java
@@ -2,13 +2,41 @@ public class Lideres{
 private String nombre;
 private int nivel;
 private String elemento;
-private String[] listaPokemones;
+private Pokemon[] listaPokemones;
 
 
-public Lideres(String nombre, int nivel){
-    this.nombre=nombre;
-    this.nivel=nivel;
-    this.listaPokemones=new String[6];
+    public Lideres(String nombre, int nivel, String elemento){
+        this.nombre=nombre;
+        this.nivel=nivel;
+        this.elemento=elemento;
+        this.listaPokemones=new Pokemon[6];
+
+    }
+     
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNivel(int nivel){
+        this.nivel=nivel;
+    }
+
+    public int getNivel(){
+        return nivel;
+    }
+
+    public void setElemento(String elemento){
+        this.elemento=elemento;
+    }
+
+    public String getElemento(){
+        return elemento;
+    }
+
 
 }
-}
+
