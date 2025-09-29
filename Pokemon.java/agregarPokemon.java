abe013c2eda0bc83aacc1c0c2b89378539554fd7
Chun Ping Liu Li <c5g492@ecci.ucr.ac.cr>
2025-09-29 08:45:50 -0600

    Le di nombre a los pokemons

---
+++ Pokemon.java/agregarPokemon.java
@@ -0,0 +1,24 @@
+public class agregarPokemon{
+      public agregarPokemon(){
+      Pokemon choripan = new pokemon();
+      Pokemon salamandra = new Pokemon():
+      Pokemon pejelagarto = new Pokemon();
+      Pokemon tralalerotralala = new Pokemon();
+      Pokemon laSaicaTimida = new Pokemon();
+      Pokemon GokuFaseTerminal = new Pokemon();
+      Pokemon pericrack = new Pokemon();
+      Pokemon marihuanauta = new Pokemon();
+      Pokemon paracetamol = new Pokemon();
+      Pokemon Bazuco = new Pokemon();
+      Pokemon tiaToxica = new Pokemon();
+      Pokemon Crackosaurio = new Pokemon();
+      Pokemon Fentalino = new Pokemoon ();
+      Pokemon Ketamina = new Pokemon();
+      
+
+
+
+
+      }
+
+}
