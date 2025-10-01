0c138ccf3c8db8695c2c93550acb261d7e2b6d98
Jefferson <englishreformed@gmail.com>
2025-10-01 10:02:20 -0600

    funciones en Entrenador para la IA del NPC

--- Pokemon.java/Entrenador.java
+++ Pokemon.java/Entrenador.java
@@ -1,4 +1,5 @@
 // Clase Entrenador que representa a un entrenador con su nombre, si es jugador o no, y su equipo de Pokémon
+import java.util.Random;
 public class Entrenador {
     private String nombre;
     private boolean esJugador;
@@ -33,4 +34,23 @@ public class Entrenador {
     public void setEquipo(Pokemon[] equipo) { 
         this.equipo = equipo; 
         }
+
+    public Ataque ataqueNPC(Pokemon pokemon){
+        int posicion = 0;
+        int tamaño = 0;
+        for (Ataque a : pokemon.getHabilidades()) {
+            if (a.getPp()>0){
+                tamaño += 1;
+            }
+        }
+        Ataque[] atqdis = new Ataque[tamaño];
+        for (int i = 0; i < atqdis.length ; i ++){
+            if (pokemon.getHabilidades()[i].getPp() > 0){
+                atqdis[posicion] = pokemon.getHabilidades()[i];
+                posicion += 1;
+            }
+        }
+        return atqdis[nextInt(tamaño)];
+    }
+
 }
