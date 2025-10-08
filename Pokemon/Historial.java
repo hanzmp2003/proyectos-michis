0ec37edd63923f146cd9f3edec21b61c0e4896ab
Chun <akaringouwu@gmail.com>
2025-10-07 22:18:08 -0600

    Arreglo en el objeto Historial

--- Pokemon/Historial.java
+++ Pokemon/Historial.java
@@ -1,73 +1,52 @@
 public class Historial {
     private String[][] batalla;
     public Historial(){
-        batalla = new String[31][5];
+        batalla = new String[31][5]; //31 batallas
 
     }
 
-    public void setHistorialEntrenador(int numBatalla, boolean estadoBatalla, Entrenador entrenador, Jugador jugador, Lideres lider) {
-        if (numBatalla < 0 || numBatalla >= batalla.length) {
-            System.out.println("Número de batalla fuera de rango.");
-            return;
+    public void registrarBatalla(int numBatalla, boolean victoria, String rival, Pokemon[] equipoRival, Jugador jugador) {
+        batalla[numBatalla][0] = victoria ? "Victoria" : "Derrota";
+        batalla[numBatalla][1] = rival;
+
+        String equipoRivalStr = "";
+        for (Pokemon p : equipoRival) {
+            equipoRivalStr += p.getNombre() + " ";
         }
+        batalla[numBatalla][2] = equipoRivalStr;
 
-        if (numBatalla != 6 && numBatalla != 7 && numBatalla != 8 &&
-            numBatalla != 16 && numBatalla != 17 && numBatalla != 18 &&
-            numBatalla != 19 && numBatalla != 27 && numBatalla != 28 &&
-            numBatalla != 29 && numBatalla != 30 && numBatalla != 31) {
+        batalla[numBatalla][3] = jugador.getNombre();
 
-            batalla[numBatalla][0] = estadoBatalla ? "Victoria" : "Derrota";
-            batalla[numBatalla][1] = entrenador.getNombre();
-            System.out.printf("Resultado: %s%n", batalla[numBatalla][0]);
-            System.out.printf("Nombre del Rival: %s%n", batalla[numBatalla][1]);
-
-            String equipoStr = "";
-            System.out.println("Equipo de Pokémon del Rival:");
-            for (Pokemon pokemon : entrenador.getEquipo()) {
-                System.out.println("- " + pokemon.getNombre());
-                equipoStr += pokemon.getNombre() + " ";
-            }
-            batalla[numBatalla][2] = equipoStr;
-
-            batalla[numBatalla][3] = jugador.getNombre();
-            System.out.printf("Nombre del Jugador: %s%n", batalla[numBatalla][3]);
-
-            String equipoJugadorStr = "";
-            System.out.println("Equipo de Pokémon del Jugador:");
-            for (Pokemon pokemon : jugador.getEquipo()) {
-                System.out.println("- " + pokemon.getNombre());
-                equipoJugadorStr += pokemon.getNombre() + " ";
-            }
-            batalla[numBatalla][4] = equipoJugadorStr;
-
-        } else {
-            batalla[numBatalla][0] = estadoBatalla ? "Victoria" : "Derrota";
-            batalla[numBatalla][1] = lider.getNombre();
-            System.out.printf("Resultado: %s%n", batalla[numBatalla][0]);
-            System.out.printf("Nombre del Rival: %s%n", batalla[numBatalla][1]);
-
-            String equipoStr = "";
-            System.out.println("Equipo de Pokémon del Rival:");
-            for (Pokemon pokemon : lider.getEquipo()) {
-                System.out.println("- " + pokemon.getNombre());
-                equipoStr += pokemon.getNombre() + " ";
-            }
-            batalla[numBatalla][2] = equipoStr;
-
-            batalla[numBatalla][3] = jugador.getNombre();
-            System.out.printf("Nombre del Jugador: %s%n", batalla[numBatalla][3]);
-
-            String equipoJugadorStr = "";
-            System.out.println("Equipo de Pokémon del Jugador:");
-            for (Pokemon pokemon : jugador.getEquipo()) {
-                System.out.println("- " + pokemon.getNombre());
-                equipoJugadorStr += pokemon.getNombre() + " ";
-            }
-            batalla[numBatalla][4] = equipoJugadorStr;
+        String equipoJugadorStr = "";
+        for (Pokemon p : jugador.getEquipo()) {
+            equipoJugadorStr += p.getNombre() + " ";
         }
+        batalla[numBatalla][4] = equipoJugadorStr;
     }
 
-    public String[] getBatalla(int numBatalla) {
-        return batalla[numBatalla];
+    public void mostrarResumenGimnasio(String nombreGimnasio, int[] indicesBatalla) {
+        int victorias = 0;
+        int derrotas = 0;
+
+        System.out.printf("\nHistorial del %s:\n", nombreGimnasio);
+        for (int i : indicesBatalla) {
+            if (batalla[i][0] != null) {
+                System.out.printf("\nBatalla %d:\n", i + 1);
+                System.out.println("Resultado: " + batalla[i][0]);
+                System.out.println("Rival: " + batalla[i][1]);
+                System.out.println("Equipo del Rival: " + batalla[i][2]);
+                System.out.println("Jugador: " + batalla[i][3]);
+                System.out.println("Equipo del Jugador: " + batalla[i][4]);
+
+                if (batalla[i][0].equals("Victoria")) victorias++;
+                else if (batalla[i][0].equals("Derrota")) derrotas++;
+            } else {
+                System.out.printf("\nBatalla %d: No registrada\n", i + 1);
+            }
+        }
+
+        System.out.println("\n Resumen:");
+        System.out.println("Victorias: " + victorias);
+        System.out.println("Derrotas: " + derrotas);
     }
 }
