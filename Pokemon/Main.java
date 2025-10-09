1a2dbf6d946df60fdc50b302580ed5d9795c2919
EmaLambda <englishreformed@gmail.com>
2025-10-08 19:14:55 -0600

    Documentación para Main.java

--- Pokemon/Main.java
+++ Pokemon/Main.java
@@ -1,4 +1,22 @@
+
+/**
+ * Esta es la clase principal que inicia el programa y crea una instancia del menú de prueba.
+ * 
+ * @author Hanz Madrigal Porras
+ * @author Emanuel Sancho Sánchez
+ * @author Chun Ping Liu Li
+ * @author Jefferson Miranda Sabala
+ * 
+ * @version 1.0
+ *  
+ */
 public class Main {
+    /**
+     * El poderosísimo y célebre método main, que sirve como el punto de entrada del programa. Crea una instancia del menú de prueba,
+     * lo que, debido a la construcción del constructor, inicia la interacción con el usuario.
+     * 
+     * @param args Argumentos de línea de comandos (no se utilizan en este programa).
+     */
     public static void main(String[] args) {
         MenuPrueba menu = new MenuPrueba();
     }
