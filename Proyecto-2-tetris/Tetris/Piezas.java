61349a89cbf35ea2ea1ae9bd3bd9005310c6c14c
Chun <akaringouwu@gmail.com>
2025-10-29 16:08:34 -0600

    Hice una clase para crear las piezas y añadirle color

---
+++ Proyecto-2-tetris/Tetris/Piezas.java
@@ -0,0 +1,24 @@
+public class Piezas {
+
+    //Colores usando ANSI
+        public static final String RESET = "\u001B[0m"; //para reiniciar cada que se crea una pieza
+        public static final String[] COLORS = {
+        "\u001B[0m",      // 0 - vacío
+        "\u001B[36m",     // 1 - cyan
+        "\u001B[34m",     // 2 - azul
+        "\u001B[33m",     // 3 - amarillo
+        "\u001B[32m",     // 4 - verde
+        "\u001B[35m",     // 5 - magenta
+        "\u001B[31m",     // 6 - rojo
+        "\u001B[93m"      // 7 - naranja brillante
+        };
+
+    public Piezas(){
+    String 
+
+
+    }
+
+
+    
+}
