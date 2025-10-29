8770c1da3f61d2a0816b780aaaca578408e54948
Chun <akaringouwu@gmail.com>
2025-10-29 16:12:16 -0600

    Salen cambios

--- Proyecto-2-tetris/Tetris/Piezas.java
+++ Proyecto-2-tetris/Tetris/Piezas.java
@@ -2,7 +2,7 @@ public class Piezas {
 
     //Colores usando ANSI
         public static final String RESET = "\u001B[0m"; //para reiniciar cada que se crea una pieza
-        public static final String[] COLORS = {
+        public static final String[] COLORES = {
         "\u001B[0m",      // 0 - vacío
         "\u001B[36m",     // 1 - cyan
         "\u001B[34m",     // 2 - azul
@@ -14,11 +14,48 @@ public class Piezas {
         };
 
     public Piezas(){
-    String 
+    boolean [][] piezaI = new boolean[4][1];      //Pieza I
+        for (int i = 0; i < piezaI.length; i++) {
+        for (int j = 0; j < piezaI[i].length; j++) {
+        piezaI[i][j] = true;
+    }    
+
+
+
+    String [][] piezaO = new String[2][4];      //Pieza O
+    String [][] piezaS = new String[2][3];      //Pieza S
+    String [][] piezaJ = new String[3][2];      //Pieza J
+    String [][] piezaL = new String[3][2];      //Pieza L
+    String [][] piezaZ = new String[2][3];      //Pieza Z
+    String [][] piezaT = new String[2][3];      //Pieza T
+
 
 
     }
+  }
 
+
+    public String[][] CrearPieza(boolean[][] pieza, int filaPieza, int columnaPieza){
+    String[][] piezaColor = new String[filaPieza][columnaPieza];
+
+         for (int i = 0; i < pieza.length; i++) {
+         for (int j = 0; j < pieza[i].length; j++) {
+            if(pieza[i][j]){   //Si la pieza es verdadera, entonces obtiene un bloque de color
+                piezaColor[i][j] = COLORES[1] + "██" + RESET;
+                System.out.print(COLORES[1] + "██" + RESET);  //Asigno un color por defecto, luego lo cambio con un random
+
+                }else{
+
+
+                }
+
+  
+                } 
+
+            }
+
+           return piezaColor;   
+        }
 
     
 }
