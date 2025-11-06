1d3d52759ce332c45a43149f6b0cdeda3784ff26
Emanuel287 <emanuel.sancho@ucr.ac.cr>
2025-11-05 23:07:47 -0600

    Documentación de la clase Sonido

--- Proyecto-2-tetris/Tetris/Sonido.java
+++ Proyecto-2-tetris/Tetris/Sonido.java
@@ -1,3 +1,13 @@
+/**
+ * Se encarga de reproducir los efectos de sonido en el tetris
+ * 
+ * @author Hanz Madrigal Porras, C4G754
+ * @author Chun Ping Liu Li, C5G492
+ * @author Emanuel Sancho Sánchez, C07332
+ * @author Jefferson Emanuel Miranda Sabala, C24874
+ *
+ * @version 1.0 
+ */
 package Tetris;
 
 import java.io.File;
@@ -6,6 +16,10 @@ import javax.sound.sampled.*;
 
 public class Sonido {
 
+    /**
+     * Reproduce un archivo de sonido 
+     * @param archivo  es la ruta del archivo de sonido a reproducir
+     */
     public static void reproducir(String archivo) {
         try {
             File file = new File(archivo);
