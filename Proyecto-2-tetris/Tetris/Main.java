6b812de311f8709fa118ef6a616741e33da8945f
Emanuel287 <emanuel.sancho@ucr.ac.cr>
2025-11-05 20:08:18 -0600

    Documentación del método Main

--- Proyecto-2-tetris/Tetris/Main.java
+++ Proyecto-2-tetris/Tetris/Main.java
@@ -1,5 +1,20 @@
+
+/**
+ * Esta es la clase principal que inicia el programa 
+ * 
+ * @author Hanz Madrigal Porras, C4G754
+ * @author Chun Ping Liu Li, C5G492
+ * @author Emanuel Sancho Sánchez, C07332
+ * @author Jefferson Emanuel Miranda Sabala, C24874
+ *
+ * @version 1.0 
+ */
 package Tetris;
 
+/**
+ * Es el punto de partida del programa, acá se inicia
+ * @param args Argumento de línea  de comandos (no se utiliza en este programa)
+ */
 public class Main {
     public static void main(String[] args) {
         Sonido.reproducir("Paquete_sonidos/SFX_GameStart.wav");
