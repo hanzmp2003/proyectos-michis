7b80c9b7d7dc7a6a9c9a0100e7d15b9e88704d88
Emanuel287 <emanuel.sancho@ucr.ac.cr>
2025-11-05 19:55:53 -0600

    Documentación de CrearPiezas

--- Proyecto-2-tetris/Tetris/CrearPiezas.java
+++ Proyecto-2-tetris/Tetris/CrearPiezas.java
@@ -1,5 +1,6 @@
 /**  
- * Clase CrearPieza se encarga de 
+ * Clase CrearPieza se encarga de contruir todas las piezas del tetris usando matrices booleanas y 
+ * asignarle colores  
  * 
  * @author Hanz Madrigal Porras, C4G754
  * @author Chun Ping Liu Li, C5G492
@@ -11,10 +12,17 @@
 
 package Tetris;
 
-public class CrearPiezas { //se encarga de construir todas las formas y asignarles colores.
+public class CrearPiezas {
 
-    //Colores usando ANSI
-        public static final String RESET = "\u001B[0m"; //para reiniciar cada que se crea una pieza
+        /**
+         * Para reiniciar los colores de ANSI al valor por defecto
+         */
+    
+        public static final String RESET = "\u001B[0m"; 
+
+        /**
+         * Lista de colores usando ANSI
+         */
         public static final String[] COLORES = {
         "\u001B[0m",      // 0 - vacío
         "\u001B[36m",     // 1 - cyan
@@ -26,9 +34,17 @@ public class CrearPiezas { //se encarga de construir todas las formas y asignarl
         "\u001B[93m"      // 7 - naranja brillante
         };
 
-    public Piezas[] todasLasPiezas;  //Creo una lista para almacenar todas las formas para el tetris
+    /**
+     * Arreglo en el que se almacenan todas las piezas
+     */
+    public Piezas[] todasLasPiezas;  
     //Probablemente cambie los colores luego, con un random para que no sean los mismos siempre
 
+
+    /** 
+     * Acá está el onstructor qeu inicializa cada una de la piezas del tetris con su forma y color
+     * respectivamente
+     */
     public CrearPiezas(){
         todasLasPiezas = new Piezas[] {
             new Piezas(crearI(), COLORES[1]),
@@ -41,7 +57,13 @@ public class CrearPiezas { //se encarga de construir todas las formas y asignarl
         };
 
     }
-    //Pieza I : 0
+
+
+    /**
+     * Se crea la pieza I 
+     * @return una matriz 4x4 que da la forma I
+     */
+
     private boolean[][] crearI() {
         return new boolean[][] {
             {false,false,false,false},
@@ -51,7 +73,11 @@ public class CrearPiezas { //se encarga de construir todas las formas y asignarl
         };
     }
 
-    //Pieza O : 1
+    /**
+     * Se crea la pieza O 
+     * color cyan
+     * @return una matriz 2x2 que da la forma O
+     */
     private boolean[][] crearO() {
         return new boolean[][] {
             {true, true},
@@ -59,7 +85,12 @@ public class CrearPiezas { //se encarga de construir todas las formas y asignarl
         };
     }
 
-    //Pieza S : 2
+    
+    /**
+     * Se crea la pieza S
+     * color azul 
+     * @return una matriz 3x3 que da la forma S
+     */
     private boolean[][] crearS() {
         return new boolean[][] {
             {false, true, true},
@@ -68,7 +99,12 @@ public class CrearPiezas { //se encarga de construir todas las formas y asignarl
         };
     }
     
-    //Pieza Z : 3
+    
+    /**
+     * Se crea la pieza Z
+     * color amarillo 
+     * @return una matriz 3x3 que da la forma Z
+     */
     private boolean[][] crearZ() {
         return new boolean[][] {
             {true,  true, false},
@@ -77,7 +113,11 @@ public class CrearPiezas { //se encarga de construir todas las formas y asignarl
         };
     }
 
-    //Pieza L : 4
+    /**
+     * Se crea la pieza L
+     * color verde
+     * @return una matriz 3x3 que da la forma L
+     */
     private boolean[][] crearL() {
         return new boolean[][] {
             {true, false,false},
@@ -86,7 +126,13 @@ public class CrearPiezas { //se encarga de construir todas las formas y asignarl
         };
     }
 
-    //Pieza J : 5
+    
+    /**
+     * Se crea la pieza J 
+     * color magenta 
+     * @return una matriz 3x3 que da la forma J
+     */
+
     private boolean[][] crearJ() {
         return new boolean[][] {
             {false, false,true},
@@ -95,7 +141,12 @@ public class CrearPiezas { //se encarga de construir todas las formas y asignarl
         };
     }
 
-    //Pieza T : 6
+    
+    /**
+     * Se crea la pieza T 
+     * color rojo
+     * @return una matriz 3x3 que da la forma T
+     */
     private boolean[][] crearT() {
         return new boolean[][] {
             {false, true, false},
@@ -104,13 +155,21 @@ public class CrearPiezas { //se encarga de construir todas las formas y asignarl
         };
     }
 
-    // Obtener una pieza específica (Por si necesitan escoger una pieza específica :) )
+    
+    /**
+     * Acá se devuelve una pieza especifica según el índice
+     * @param indiceForma es el índice de la pieza en el arreglo 
+     * @return la pieza solicitada y devuelve null si el índice no es el correcto
+     */
     public Piezas getPieza(int indiceForma) {
         if (indiceForma < 0 || indiceForma >= todasLasPiezas.length) return null;
         return todasLasPiezas[indiceForma];
     }
 
-    // Obtener una pieza aleatoria
+    /**
+     * De las piezas disponibles toma una aleatroriamente y la devuelve
+     * @return una pieza al azar de las que están en el arreglo
+     */
     public Piezas getPiezaAleatoria() {
         int random = (int)(Math.random() * todasLasPiezas.length);
         return todasLasPiezas[random];
